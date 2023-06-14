ebf14caeaa2e132157e0a76ea824f2f3f21d8865
Sergey <s.spiridonovv@mail.ru>
2023-06-14 12:50:25 +0300

    event tabs complete

--- wp-content/themes/wizard-new/templates/all-events.php
+++ wp-content/themes/wizard-new/templates/all-events.php
@@ -54,34 +54,48 @@
       'order' => 'ASC'
     );
     $events_all = get_posts($args_all);
+
+    $args_tab = array(
+      'post_type' => 'events',
+      'posts_per_page' => -1,
+      'orderby' => "title",
+    );
+    $eventsTabs_all = get_posts($args_tab);
+
+    $tabArr = [];
     query_posts($args_all);
     wp_reset_postdata();
     ?>
 
-    <?php
-    //Вывод категорий по id родительской категории
-    $cat_args = array(
-      'hide_empty' => true, //скрывать пустые
-      'depth'      => 1, //глубина просмотра
-      'child_of'    => 18 //id родительской категории
-    );
-    ?>
-
-
+    <!-- Табы мероприятий -->
     <div class="grid col7-3-2 m-b-20 tabslink" id="flavor-nav">
       <a rel="all" class="current">Все мероприятия</a>
       <?php
-      //
-      $event_categories = get_terms('category', $cat_args);
+      if (!empty($eventsTabs_all)) {
 
-      foreach ($event_categories as $event_cat) {
+        foreach ($eventsTabs_all as $event) {
 
-        $item = '<a rel="' . $event_cat->slug . '" class="">'  . $event_cat->name . '</a>';
-        echo $item;
+          $eventDateTab = strtotime(get_field('data_meropriyatiya', $event->ID));
+          $dateNowTab = strtotime(date('d.m.Y'));
+          $tabTerms = get_field('kategoriya_meropriyatiya', $event->ID);
+
+          if ($eventDateTab >= $dateNowTab) {
+            foreach ($tabTerms as $term) {
+              $tabItem = '<a rel="' . $term->slug . '" class="">'  . $term->name . '</a>';
+              $tabArr[] = $tabItem;
+            }
+          }
+        }
+
+        $resultTab = array_unique($tabArr);
+        foreach ($resultTab as $resultTabSngl) {
+          echo $resultTabSngl;
+        }
       }
       ?>
     </div>
 
+    <!-- Активные и будущие мероприятия -->
     <div id="all-flavors" class="grid col4-2-1 gap10 news-list calendar">
       <?php if (!empty($events_all)) : ?>
         <?php foreach ($events_all as $event) { ?>
@@ -114,12 +128,7 @@
           }
           $terms = get_field('kategoriya_meropriyatiya', $event->ID);
           if ($eventDate >= $dateNow) { ?>
-            <a data-tab="<?php if ($terms) {
-                                                                              foreach ($terms as $term) {
-                                                                                echo esc_html($term->slug);
-                                                                              }
-                                                                            }
-                                                                            ?>" href="<?php the_permalink($event->ID); ?>" class="flavor all <?php if ($terms) {
+            <a href="<?php the_permalink($event->ID); ?>" class="flavor all <?php if ($terms) {
                                                                               foreach ($terms as $term) {
                                                                                 echo esc_html($term->slug);
                                                                               }
@@ -142,14 +151,61 @@
       <?php endif; ?>
 
     </div>
+
+    <br>
+    <h1>Прошедшие мероприятия</h1>
+    <div class="grid col4-2-1 gap10 news-list calendar">
+      <?php if (!empty($events_all)) : ?>
+        <?php foreach ($events_all as $event) { ?>
+          <?php
+          $eventDate = strtotime(get_field('data_meropriyatiya', $event->ID));
+          $dateNow = strtotime(date('d.m.Y'));
+          ?>
+
+          <?php
+          $eventType = get_field('tip_meropriyatiya', $event->ID);
+          switch ($eventType) {
+            case 'Курсы':
+              $eventTypeImg = '<span class="material-icons">school</span>';
+              break;
+            case 'Вебинар':
+              $eventTypeImg = '<span class="material-icons">cast</span>';
+              break;
+            case 'Онлайн-уроки':
+              $eventTypeImg = '<span class="material-icons">cast_for_education</span>';
+              break;
+            case 'Интенсивы':
+              $eventTypeImg = '<span class="material-icons">rocket_launch</span>';
+              break;
+            case 'Семинары':
+              $eventTypeImg = '<span class="material-icons">group</span>';
+              break;
+            case 'Конференции':
+              $eventTypeImg = '<span class="material-icons">groups</span>';
+              break;
+          }
+          $terms = get_field('kategoriya_meropriyatiya', $event->ID);
+          if ($eventDate < $dateNow) { ?>
+            <a href="<?php the_permalink($event->ID); ?>" class="">
+              <div class="grid col2-2">
+                <div><?php the_field('data_meropriyatiya', $event->ID); ?></div>
+                <div>Организатор: <?php the_field('organizator', $event->ID); ?></div>
+              </div>
+              <p>
+                <?php echo $eventTypeImg; ?>
+                <?php the_field('nazvanie', $event->ID); ?>
+              </p>
+              <?php the_field('opisanie_dlya_prevyu', $event->ID); ?>
+            </a>
+          <?php    }
+          ?>
+
+        <?php  } ?>
+      <?php endif; ?>
+
+    </div>
   </div>
 </section>
-
-
-
-
-
-<?php get_footer(); ?>
 
 <script>
   $(function() {
@@ -167,32 +223,4 @@
     });
   });
 </script>
-
-<!-- TODO: -->
-<!-- Удалять таб мероприятия, если мероприятия этой категории больше нет -->
-<script>
-  document.addEventListener("DOMContentLoaded", function(event) {
-    // Табы навигации
-    // const tabsLink = document.querySelectorAll('.tabslink a')
-    // tabsLink.forEach(function(tabLink) {
-    //   console.log(tabLink);
-    // })
-
-    // const eventItems = document.querySelectorAll('#all-flavors a')
-    // eventItems.forEach(function(eventItem) {
-    //   console.log(eventItem);
-
-    //   switch(eventItem) {
-    //     case eventItem.dataset.tab === 'konferenczii':
-    //       finfo_file
-    //       break;
-    //   }
-
-    //   // if (eventItem.dataset.tab === 'konferenczii') {
-    //   //   console.log(true);
-    //   // }
-    // })
-
-
-  });
-</script>
+<?php get_footer(); ?>
